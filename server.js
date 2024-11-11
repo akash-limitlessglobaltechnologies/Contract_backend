67704ed34cbf62f62449471de50b0a8a4fbd1116
Akash <akash.b@limitlessglobaltechnologies.com>
2024-11-11 16:08:55 +0530

    fifth commit

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-// Session configuration
+
+
 app.use(expressSession({
     secret: process.env.SESSION_SECRET,
     resave: false,
